cf24f94ff333c5c36795d5ecb845022a20c0bae2
TypeErrorDev <matthew.pantel86@gmail.com>
2022-07-15 00:06:54 +0300

    ADD: Working on the activities costs updates

--- js/script.js
+++ js/script.js
@@ -1,36 +1,44 @@
+/* ==================================
+              VARIABLES
+===================================== */
+// nameFocus
 const nameFocus = document.querySelector("#name");
+// Job Role
 const jobRoleSelect = document.querySelector("#title");
 const otherJobRoleInput = document.querySelector("#other-job-role");
+// T-Shirt selections
 const designSelect = document.querySelector("#design");
 const colorSelect = document.querySelector("#color");
 const colorOption = colorSelect.children;
+// Activities
+const activitiesBox = document.getElementById("activities");
+const activitiesCheckbox = document.querySelectorAll('[type="checkbox"]');
+const activitiesCost = document.getElementById("activities-cost");
 
-/////////////////////////////////////////////////////////////////
-/////////////// FOCUS ON NAME INPUT ON PAGE LOAD ////////////////
-/////////////////////////////////////////////////////////////////
-
+/* ==================================
+    FOCUS ON NAME INPUT ON PAGE LOAD
+===================================== */
 nameFocus.focus();
 
-/////////////////////////////////////////////////////////////////
-/////////////// HIDING THE OTHER JOB ROLE FIELD /////////////////
-/////////////////////////////////////////////////////////////////
+/* ==================================
+    OTHER JOB ROLE DISABLED ON LOAD
+===================================== */
 
-function hideOtherJobRole() {
-  otherJobRoleInput.style.display = "none";
+otherJobRoleInput.style.display = "none";
 
-  jobRoleSelect.addEventListener("change", (e) => {
-    if (e.target.value === "other") {
-      otherJobRoleInput.style.display = "initial";
-    } else {
-      otherJobRoleInput.style.display = "none";
-    }
-  });
-}
-hideOtherJobRole();
+/* ==================================
+    HIDING THE OTHER JOB ROLE FIELD
+===================================== */
 
-/////////////////////////////////////////////////////////////////
-//////////////// T-SHIRT SELECT BOX INTERACTION /////////////////
-/////////////////////////////////////////////////////////////////
+jobRoleSelect.addEventListener("change", (e) => {
+  e.target.value === "other"
+    ? (otherJobRoleInput.style.display = "initial")
+    : (otherJobRoleInput.style.display = "none");
+});
+
+/* ==================================
+    T-SHIRT SELECT BOX INTERACTION
+===================================== */
 
 // T-Shirt Select Options
 function tshirtColorSelections() {
@@ -51,3 +59,21 @@ function tshirtColorSelections() {
   });
 }
 tshirtColorSelections();
+
+/* ==================================
+    ACTIVITIES TOTAL COST UPDATE
+===================================== */
+
+function compareDatesAndTimes() {}
+
+function addCosts() {}
+
+activitiesBox.addEventListener("change", (e) => {
+  const total = activitiesCost;
+  let price = 0;
+  const selectedEvents = e.target;
+  activitiesCheckbox.forEach((field) => {
+    let label = field.parentElement;
+    console.log(label);
+  });
+});
