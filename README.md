548b08010e50248a6dbf8efdfef048415109d517
Matthew Pantel <79867801+TypeErrorDev@users.noreply.github.com>
2022-07-22 00:32:08 +0300

    ADD:  Form Validation, Not Finished
    created payment validation, working on form submit validation. Need to finetune regex usage though. Seems to not want to work

--- README.md
+++ README.md
@@ -1,2 +1,98 @@
-# FSJS-3-Interactive-Form
- 
+# Team Treehouse Full Stack JavaScript Project 3 - Interactive Form
+
+In this project, I have used JavaScript to enhance an interactive registration form for a fictional Full Stack conference.
+
+## Table of contents
+
+- [Overview](#overview)
+  - [Screenshot](#screenshot)
+  - [Links](#links)
+- [My process](#my-process)
+  - [Built with](#built-with)
+  - [What I learned](#what-i-learned)
+  - [Continued development](#continued-development)
+  - [Useful resources](#useful-resources)
+- [Author](#author)
+- [Acknowledgments](#acknowledgments)
+
+## Overview
+
+### The challenge
+
+Users should be able to:
+
+- Create, Read, Update, and Delete markdown documents
+- Name and save documents to be accessed as needed
+- Edit the markdown of a document and see the formatted preview of the content
+- View a full-page preview of the formatted content
+- View the optimal layout for the app depending on their device's screen size
+- See hover states for all interactive elements on the page
+- **Bonus**: If you're building a purely front-end project, use localStorage to save the current state in the browser that persists when the browser is refreshed
+- **Bonus**: Build this project as a full-stack application
+
+### Screenshot
+
+![](./screenshot.jpg)
+
+Add a screenshot of your solution. The easiest way to do this is to use Firefox to view your project, right-click the page and select "Take a Screenshot". You can choose either a full-height screenshot or a cropped one based on how long the page is. If it's very long, it might be best to crop it.
+
+Alternatively, you can use a tool like [FireShot](https://getfireshot.com/) to take the screenshot. FireShot has a free option, so you don't need to purchase it.
+
+Then crop/optimize/edit your image however you like, add it to your project, and update the file path in the image above.
+
+**Note: Delete this note and the paragraphs above when you add your screenshot. If you prefer not to add a screenshot, feel free to remove this entire section.**
+
+### Links
+
+- Live Site URL: [Add live site URL here](https://your-live-site-url.com)
+
+## My process
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- Flexbox
+- CSS Grid
+- Mobile-first workflow
+- JavaScript
+
+### What I learned
+
+While working on this project, I solidified my knowledge with using regex to validate form inputs and learned how to manipulate <option> elements. Below
+
+To see how you can add code snippets, see below:
+
+```html
+<h1>Some HTML code I'm proud of</h1>
+```
+
+```css
+.proud-of-this-css {
+  color: papayawhip;
+}
+```
+
+```js
+const proudOfThisFunc = () => {
+  console.log("🎉");
+};
+```
+
+If you want more help with writing markdown, we'd recommend checking out [The Markdown Guide](https://www.markdownguide.org/) to learn more.
+
+**Note: Delete this note and the content within this section and replace with your own learnings.**
+
+### Useful resources
+
+- [Regexr](https://regexr.com/) - This site has helped me with testing my regex and ensuring that I'm validating the appropriate characters
+
+## Author
+
+- Website - [Matthew Pantel](https://www.matthewpantel.com)
+- LinkedIn - [/MatthewPantel](https://www.LinkedIn.com/in/MatthewPantel)
+- Twitter - [@TypeErrorDev](https://www.twitter.com/TypeErrorDev)
+
+## Acknowledgments
+
+I'd like to give thanks to the Student Success Mentors with Team Treehouse, the other students in their Slack channel and my friends on Discord that helped me along the way. Without everyone's support, I'd still be figuring out how to use Regex to validate this registration form.
--- js/script.js
+++ js/script.js
@@ -11,10 +11,29 @@ const designSelect = document.querySelector("#design");
 const colorSelect = document.querySelector("#color");
 const colorOption = colorSelect.children;
 // Activities
-const activities = document.getElementById("activities");
+const activities = document.getElementById("activities-box");
 const checkboxes = activities.querySelectorAll("input[type=checkbox]");
 let finalCost = document.querySelector("#activities-cost");
 let totalCost = 0;
+// Payment
+const paypalSelection = document.getElementById("paypal");
+const bitcoinSelection = document.getElementById("bitcoin");
+const paymentSelection = document.getElementById("payment");
+const creditCard = paymentSelection[1];
+// Form Validation
+const nameRegex = /[^a-zA-Z\ ]+/g;
+const emailRegex = /^[^\@]+@[^\@]+\.[^\@]+$/;
+const formValidation = document.querySelector("form");
+const nameInput = document.getElementById("name");
+const emailInput = document.getElementById("email");
+const emailHint = document.getElementById("email-hint");
+// CC Validation
+const ccInput = document.getElementById("cc-num");
+const zipInput = document.getElementById("zip");
+const cvvInput = document.getElementById("cvv");
+const ccRegex = /^[0-9]{13,16}$/;
+const zipRegex = /^[0-9]{5}$/;
+const cvvRegex = /^[0-9]{3}$/;
 
 /* ==================================
     FOCUS ON NAME INPUT ON PAGE LOAD
@@ -24,13 +43,11 @@ nameFocus.focus();
 /* ==================================
     OTHER JOB ROLE DISABLED ON LOAD
 ===================================== */
-
 otherJobRoleInput.style.display = "none";
 
 /* ==================================
     HIDING THE OTHER JOB ROLE FIELD
 ===================================== */
-
 jobRoleSelect.addEventListener("change", (e) => {
   e.target.value === "other"
     ? (otherJobRoleInput.style.display = "initial")
@@ -40,7 +57,6 @@ jobRoleSelect.addEventListener("change", (e) => {
 /* ==================================
     T-SHIRT SELECT BOX INTERACTION
 ===================================== */
-
 // T-Shirt Select Options
 function tshirtColorSelections() {
   colorSelect.disabled = true;
@@ -64,7 +80,6 @@ tshirtColorSelections();
 /* ==================================
     ACTIVITIES TOTAL COST UPDATE
 ===================================== */
-
 activities.addEventListener("change", (e) => {
   let activity = e.target;
   let activity_cost = parseInt(activity.getAttribute("data-cost"));
@@ -90,23 +105,101 @@ activities.addEventListener("change", (e) => {
     ? (totalCost += activity_cost)
     : (totalCost -= activity_cost);
 
-  finalCost.innerHTML = `Total: $ ${totalCost}`;
+  finalCost.innerHTML = `Total: $${totalCost}`;
 });
 
 /* ==================================
         PAYMENT INFO SECTION
 ===================================== */
-const paypalSelection = document.getElementById("paypal");
-const bitcoinSelection = document.getElementById("bitcoin");
-const paymentSelection = document.getElementById("payment");
-const creditCard = paymentSelection[1];
-
 function paymentDefaults() {
   if ((creditCard.selected = true)) {
     paypalSelection.classList.add("hidden");
     bitcoinSelection.classList.add("hidden");
-  } else {
-    paypalSelection.classList.remove("hidden");
   }
 }
 paymentDefaults();
+paymentSelection.addEventListener("change", (e) => {
+  console.log(e.target.value);
+  if (e.target.value === "credit-card") {
+    paypalSelection.classList.add("hidden");
+    bitcoinSelection.classList.add("hidden");
+  } else if (e.target.value === "paypal") {
+    paypalSelection.classList.remove("hidden");
+    bitcoinSelection.classList.add("hidden");
+  } else if (e.target.value === "bitcoin") {
+    paypalSelection.classList.add("hidden");
+    bitcoinSelection.classList.remove("hidden");
+  }
+});
+
+/* ==================================
+        FORM VALIDATION SECTION
+===================================== */
+function isNameValid() {
+  let name = nameInput.value;
+  return nameRegex.test(name);
+}
+function isEmailValid() {
+  let email = emailInput.value;
+  return emailRegex.test(email);
+}
+function isActivitiesValid() {
+  finalCost === 0 ? false : true;
+}
+function isCreditCardValid() {
+  let cc = ccInput.value;
+  return ccRegex.test(cc);
+}
+function isZipValid() {
+  let zip = zipInput.value;
+  return zipRegex.test(zip);
+}
+function isCVVValid() {
+  let cvv = cvvInput.value;
+  return cvvRegex.test(cvv);
+}
+function isNotValid(arg) {
+  arg.parentNode.classList.add("not-valid");
+  arg.parentNode.classList.remove("valid");
+  arg.parentNode.lastElementChild.style.display = "block";
+}
+function isValid(arg) {
+  arg.parentNode.classList.add("valid");
+  arg.parentNode.classList.remove("not-valid");
+  arg.parentNode.lastElementChild.style.display = "none";
+}
+formValidation.addEventListener("keyup", (e) => {
+  if (!isNameValid()) {
+    e.preventDefault();
+    isNotValid(nameInput);
+  } else {
+    isValid(nameInput);
+  }
+});
+formValidation.addEventListener("submit", (e) => {
+  if (!isNameValid()) {
+    e.preventDefault();
+    isNotValid(nameInput);
+  } else {
+    isValid(nameInput);
+  }
+  if (!isEmailValid()) {
+    e.preventDefault();
+    let email = emailInput.value;
+    if (email === "") {
+      emailHint.textContent = `Email field is empty, please enter a valid email address.`;
+    } else if (/^\w+@\w+.\w+$/.test(email) === false) {
+      emailHint.textContent = `Your email address is missing the "@" symbol.`;
+    } else {
+      emailHint.textContent = `Email address is formatted incorrectly`;
+    }
+  } else {
+    isValid(emailInput);
+  }
+  if (!isActivitiesValid()) {
+    e.preventDefault();
+    isNotValid(activities);
+  } else {
+    isValid(activities);
+  }
+});
